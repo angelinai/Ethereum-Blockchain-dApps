965196f267dc81cd7f09bfa8d6c01e453cd11777
Angelina Ilieva <angelina.ilieva@gmail.com>
2018-08-31 07:05:24 -0600

    updated dapp ui and add deployment directives

--- FoodSafe/app/scripts/index.js
+++ FoodSafe/app/scripts/index.js
@@ -19,6 +19,10 @@ const FoodSafe = contract(foodSafeArtifact)
 // For application bootstrapping, check out window.addEventListener below.
 let accounts
 let account
+var foodSafeABI;
+var foodsafeContract;
+var foodSafeBinaryCode;
+var foodSafeCompiled;
 
 const App = {
   start: function () {
@@ -43,12 +47,25 @@ const App = {
       accounts = accs
       account = accounts[0]
 
-      
+      web3.eth.defaultAccount = account; //setup accout
 
-
-     // self.refreshBalance()
+      var foodSafeSource = "pragma solidity ^0.4.24;  contract FoodSafe {     struct Location      {          uint LocationId;         string Name;         uint PreviousLocationId;         uint Timestamp; // timestamp of the block mining          string Secret;             }      mapping(uint => Location) Trail;     uint8 TrailCount = 0;          function AddNewLocation(uint locationId, string name, string secret) public     {         Location memory newLocation;          newLocation.LocationId = locationId;         newLocation.Name = name;         newLocation.Secret = secret;         newLocation.Timestamp = block.timestamp;           if(TrailCount != 0)         {             newLocation.PreviousLocationId = Trail[TrailCount].LocationId;         }         // add          Trail[TrailCount] = newLocation;             TrailCount++;      }      function GetTrailCount() public view returns(uint8) {         return TrailCount;     }      function GetLocation(uint8 trailNumber) public view returns(string, uint, uint)     {         return (Trail[trailNumber].Name, Trail[trailNumber].LocationId, Trail[trailNumber].PreviousLocationId);     } }";
+     
+      web3.eth.compile.solidity(foodSafeSource, function(error, foodSafeCompiled){
+            foodSafeABI - foodSafeCompiled['<stdin>:FoodSafe'].info.abiDefinition;
+            foodsafeContract = web3.eth.contract(foodSafeABI);
+            foodSafeBinaryCode = foodSafeCompiled['<stdin>:FoodSafe'].code;
+        });
     })
   },
+  // upload contract to the blockchain 
+  createContract: function(){
+    foodsafeContract.new("", {from: account, data:foodSafeCode, gas:3000000}, function(error, deployedContract){
+        if(deployedContract.address){
+          document.getElementById("contractAddress").value = deployedContract.address;
+        }
+    });
+  }
 
     /*
 
